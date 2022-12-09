c170362c66e7d286bd9f4e89e2d0b51a5bf0725e
danielkuzminski <dankuzmina@gmail.com>
2022-12-09 03:14:39 +0100

    git create signup form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // pages and components
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 import Home from './pages/Home';
 import Add from './pages/Add';
 import NotFound from './pages/NotFound';
@@ -17,6 +18,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
           <Route path='/home' element={<Home />} />
           <Route path='/add_game' element={<Add />} />
           <Route path='/home/games/:id' element={<Game />} ></Route>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,22 +1,20 @@
 import { useState } from 'react'
 
-// routing
-import { Link } from 'react-router-dom'
-
 // styles
 import './Login.css'
 
-export default function Login() {
+export default function Singnup() {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
 
-  const login = async (email, password) => {}
+  const signup = async (email, password, displayName) => {}
 
   return (
     <div>
     <h1 className='main-title'>Moje gry</h1>
-    <form className='login-form' onSubmit={login}>
+    <form className='login-form' onSubmit={signup}>
       <label>
         <span className='login-title'>email: </span>
         <input 
@@ -41,8 +39,19 @@ export default function Login() {
           value={password}
            />
       </label>
+      <label>
+        <span className='login-title'>user name: </span>
+        <input 
+          className='login-input'
+          type="text"
+          required
+          onChange={(e) => {
+            setDisplayName(e.target.value)
+          }} 
+          value={displayName}
+          />
+      </label>
       <button className='login-button' type='submit'>zaloguj</button>
-      <Link className='login-button signup' to={'/signup'}>Zarejestruj się</Link>
     </form>
     </div>
   )
